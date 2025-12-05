1fc59f4b1c4fd79d946e113ac90f98cf6a4333be
Sidny8 <telc090311mgtnpna1@cbtis217.edu.mx>
2025-12-04 18:38:44 -0600

    Base iniciada para la creacion del login

---
+++ TrabajoExamen/TrabajoExamen/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC40
+ * Date: 04/12/2025
+ * Time: 06:37 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TrabajoExamen
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
